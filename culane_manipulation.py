b5766ee279236fc671d70aee7a300c36f8faa394
Amiiiiine <ka_adjou@esi.dz>
2023-09-11 23:42:05 +0100

    Add CULane_Dataset & some manipulations

---
+++ culane_manipulation.py
@@ -0,0 +1,63 @@
+
+import cv2
+import matplotlib.pyplot as plt
+from culaneDataset import CULane
+from torch.utils.data import DataLoader
+
+
+#constants
+data_root = '/mnt/Travail/DLProjects/RSC/LaneRSC/datasets/culane'
+
+
+train_dataset = CULane(data_root, 'train')
+test_dataset = CULane(data_root, 'test')
+
+print(train_dataset.__len__())
+print(test_dataset.__len__())
+
+
+#Init Dataset loader
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=train_dataset.collate, num_workers=8)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True, collate_fn=test_dataset.collate, num_workers=8)
+
+
+
+
+#show a sample x from culane, (x ~ [0-3376] for testing dataset / x ~  [0-18232] for training dataset )
+
+def show_culane_sample(Dataset, idx):
+    img_path = Dataset.__getitem__(idx)['img_name']
+    img = plt.imread(img_path)
+    # plt.figure(figsize=(16, 18))
+    # plt.imshow(img)  # showing row image.
+
+    anno_path = img_path[:-3] + 'lines.txt'  # remove sufix jpg and add lines.txt
+
+    exist_list = [] # contains [x, y] for all key points that belong to lanes in the image...all annotations.
+    with open(anno_path) as f:
+        for line in f:
+            line = line.strip()
+            l = line.split(" ")
+            exist_list.append([int(eval(x)) for x in l[2:]])
+
+    print(exist_list)
+
+    for j in range(0, exist_list.__len__()):
+        for i in range(0, exist_list[j].__len__(), 2):
+            image = cv2.circle(img, (exist_list[j][i], exist_list[j][i + 1]), radius=0, color=(0, 255, 0), thickness=10)
+            img = image
+
+    plt.figure(figsize=(16, 18))
+    plt.imshow(image)
+    plt.show()
+
+
+
+
+
+show_culane_sample(train_dataset, 1800)
+show_culane_sample(test_dataset, 1800)
+
+
+
+
